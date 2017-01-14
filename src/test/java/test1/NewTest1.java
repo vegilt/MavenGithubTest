e29948690f240670923711b4c9343e17c3525416
vegilt <vegil88@gmail.com>
2017-01-14 23:52:22 +0530

    Commiting again
    Commiting again

---
+++ src/test/java/test1/NewTest1.java
@@ -0,0 +1,12 @@
+package test1;
+
+import org.testng.annotations.Test;
+
+public class NewTest1 {
+  @Test
+  public void f() 
+  {
+	  System.out.println("Git Hub Practice Test");
+	  System.out.println("To Test in Jaba's Machine");
+  }
+}
---
+++ src/test/java/test1/NewTest2.java
@@ -0,0 +1,9 @@
+package test1;
+
+import org.testng.annotations.Test;
+
+public class NewTest2 {
+  @Test
+  public void f() {
+  }
+}
